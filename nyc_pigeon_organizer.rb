78d386970e1f1d42e1dfeb34bee25c387e489fcf
github username <github email address>
2018-11-18 17:50:21 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organizedPigeons = {}
+  data.each do |keys, keyValues|
+      keyValues.each do |keys2, names|
+        names.each do |name|
+        if organizedPigeons[name] == nil
+          organizedPigeons[name] = {keys => [keys2.to_s]}
+        elsif organizedPigeons[name][keys] == nil
+          #binding.pry
+          organizedPigeons[name][keys] = [keys2.to_s]
+        else
+          organizedPigeons[name][keys] << keys2.to_s
+        end
+      end
+    end
+  end
+  organizedPigeons
 end
